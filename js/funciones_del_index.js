2c93d0b81b0385b16ae60544fcf5fe9049b0c58b
Juan Francisco Alvero <macaronij@gmail.com>
2024-12-16 09:14:37 -0300

    Productos con fetch

---
+++ js/funciones_del_index.js
@@ -0,0 +1,30 @@
+
+document.addEventListener("DOMContentLoaded",()=>{
+    let productosContainer = document.getElementById("productos-container");
+    fetch("https://dummyjson.com/products?limit=20")
+        .then(response => response.json())
+        .then(data => 
+            {
+                let productos = data.products;
+                productosContainer.innerHTML="";
+                productos.forEach(elemento => {
+                    const cardDiv = document.createElement("DIV");
+
+                    cardDiv.innerHTML = 
+                    `<div class="card">
+                        <img class="card-foto" src="${elemento.thumbnail}">
+                        <div class="card-titulo"><h3>${elemento.title}</h3></div>
+                        <a href="carrito.html"><button class="boton">Comprar</button></a>
+                        <div class="card-texto">${elemento.description}</div>
+                    </div>`;
+                    
+                    // const botonAgregar = cardDiv.querySelector("button");
+                    // botonAgregar.addEventListener("click", () => {
+                    //     alert("agregar producto al carrito");
+                    // })
+
+                    productosContainer.appendChild(cardDiv);
+                });
+            })
+        .catch( error => console.log("ERROR DE FETCH", error));
+})
