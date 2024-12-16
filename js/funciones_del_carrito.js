ee3ec0cbab46b61e8faa9f9964aedc4354d5998d
Juan Francisco Alvero <macaronij@gmail.com>
2024-12-16 12:25:04 -0300

    Carrito botones confrimar y cancelar compra funcionando

--- js/funciones_del_carrito.js
+++ js/funciones_del_carrito.js
@@ -1,3 +1,5 @@
+var precioTotal = 0;
+
 if (localStorage.getItem("carrito") !== null) {
     console.log("carrito encontrado");
     let carritoSTRING = localStorage.getItem("carrito");
@@ -13,5 +15,23 @@ if (localStorage.getItem("carrito") !== null) {
                          <td class="eliminar"><i class="fa-solid fa-trash"></i></td>
         `;
         carritoHTML.appendChild(linea);
+        precioTotal += elemento[1];
     })  
 }
+
+let precioHTML = document.getElementById("precioHTML");
+precioHTML.innerHTML = `$${precioTotal}`;
+
+let botonConfirmar = document.getElementById("botonConfirmar");
+botonConfirmar.addEventListener("click", () => {
+        alert("COMPRA CONFIRMADA");
+        localStorage.removeItem("carrito");
+    }
+);
+
+let botonCancelar = document.getElementById("botonCancelar");
+botonCancelar.addEventListener("click", () => {
+        alert("COMPRA CANCELADA");
+        localStorage.removeItem("carrito");
+    }
+);
